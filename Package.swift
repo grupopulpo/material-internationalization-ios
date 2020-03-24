824d8ef4ad576d76d06fd97f20b5f63a723b6d89
Guillermo Bernal <gbernal096@gmail.com>
2020-03-24 00:53:34 -0500

    SPM

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "MDFInternationalization",
+    products: [
+        .library(name: "MDFInternationalization", targets: ["MDFInternationalization"]),
+    ],
+    targets: [
+        .target(name: "MDFInternationalization", 
+                path: "Sources", 
+                publicHeadersPath:"Sources/",
+                sources: [
+                    "Sources/*.{h,m}",
+                ])
+    ]
+)
